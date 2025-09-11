f71d3e118b81419a437c457409c511ee02fab9b9
JankoooGH <jasiek32100@gmail.com>
2025-09-12 01:15:13 +0200

    Podział zadań na: tygodniowe dzienne(powtarzalne) i codzienne(pojedyncze)

--- ToDo/models.py
+++ ToDo/models.py
@@ -1,10 +1,18 @@
 from django.db import models
 
 class Task(models.Model):
+    TASK_TYPES = [
+        ("DAILY", "Daily"),
+        ("WEEKLY", "Weekly"),
+        ("ONCE", "Once"),
+    ]
+
+
     title = models.CharField(max_length=100)
     completed = models.BooleanField(default=False)
+    task_type = models.CharField(max_length=100, choices=TASK_TYPES, default='Once')
 
 
 
     def __str__(self):
-        return self.title
+        return f"{self.title} ({self.get_task_type_display()})"
